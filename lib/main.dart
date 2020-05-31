92e504a4e4abedf4f02fcc19f109b040e39c1cba
rheasharma22 <originalrhea@gmail.com>
2020-05-30 19:42:56 -0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'screen_one.dart';
+
+void main() {
+  runApp(FitnessApp());
+}
+
+class FitnessApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+      ),
+      home: ScreenOne(),
+    );
+  }
+}
---
+++ lib/screen_one.dart
@@ -0,0 +1,132 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+//import 'package:stop_watch_timer/stop_watch_timer.dart';
+import 'package:flutter_timer/flutter_timer.dart';
+
+const kAppBarStyle = TextStyle(
+  fontSize: 20.0,
+  fontWeight: FontWeight.bold,
+  letterSpacing: 1.0,
+);
+
+const kButtonTextStyle = TextStyle(
+  fontSize: 20.0,
+  fontWeight: FontWeight.bold,
+  letterSpacing: 4.0,
+  color: Colors.white,
+);
+
+const kTimerStyle = TextStyle(
+  fontSize: 45.0,
+  fontWeight: FontWeight.bold,
+  color: Colors.white,
+);
+
+class ScreenOne extends StatefulWidget {
+  @override
+  _ScreenOneState createState() => _ScreenOneState();
+}
+
+class _ScreenOneState extends State<ScreenOne> {
+  bool running = false;
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Fitness Tracker"),
+          backgroundColor: Color(0xFF0A0E21),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Stack(
+                alignment: AlignmentDirectional.center,
+                children: <Widget>[
+                  Container(
+                    width: 220.0,
+                    height: 150.0,
+                    decoration: BoxDecoration(
+                      color: Color(0x251919A2),
+                      borderRadius: BorderRadius.circular(35.0),
+                    ),
+                  ),
+                  Container(
+                    child: Center(
+                      child: TikTikTimer(
+                        backgroundColor: Colors.transparent,
+                        height: 200.0,
+                        width: 200.0,
+                        running: running,
+                        timerTextStyle: kTimerStyle,
+                        initialDate: DateTime.now(),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  //color: Color(0xFFFE5728),
+                  color: Color(0xFF19BC99),
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                margin: EdgeInsets.all(15.0),
+                padding: EdgeInsets.all(5.0),
+                width: 195.0,
+                child: FlatButton(
+                  onPressed: () {
+                    try {
+                      if (running == false)
+                        setState(() {
+                          running = true;
+                        });
+                    } on Exception {}
+                  },
+                  child: Text(
+                    "START",
+                    style: kButtonTextStyle,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                    margin: EdgeInsets.all(15.0),
+                    padding: EdgeInsets.all(5.0),
+                    width: 195.0,
+                    child: FlatButton(
+                      onPressed: () {
+                        if (running == true)
+                          setState(() {
+                            running = false;
+                          });
+                      },
+                      child: Text(
+                        "STOP",
+                        style: kButtonTextStyle,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
